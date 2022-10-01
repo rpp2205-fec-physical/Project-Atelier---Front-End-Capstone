09187b253bfae46907fd2d786f33a45694d8c7d7
Steven Moody <smoody07@gmail.com>
2022-10-01 12:59:57 -0700

    returns data on api request to products

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,32 @@
+require("dotenv").config();
 const path = require('path')
 const express = require('express');
 let app = express();
+let axios = require('axios')
 
-let port = 3000;
-
+const port = process.env.PORT;
+const auth = process.env.AUTHORIZATION;
+app.use(express.json())
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/api', (req, res) => {
-  res.send('Hello API')
+app.get('/api/products', (req, res) => {
+  // likely move this into a controller file?
+  let url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/'
+  axios.get(url, {
+    headers: {
+      'Authorization': auth
+    }
+  })
+  .then((response) => {
+    res.send(response.data);
+  });
+
 })
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
+
+
+
+
