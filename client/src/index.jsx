cd383fb8e501f0f643e6c4e3af560392c9df8009
Steven Moody <smoody07@gmail.com>
2022-10-01 14:24:05 -0700

    resolves webpack issue with process variable

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReviewContainer from './RatingsReviews/index.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
+
+  render() {
+    return (
+      <div>
+        <h1>Project Atelier</h1>
+        <ReviewContainer />
+      </div>
+    )
+  }
+
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
