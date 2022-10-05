7cbeb0e58557c88ee535fef5b9f5261b3c0dfde9
Matt Alexander <dmalexander6@gmail.com>
2022-10-05 15:50:00 -0600

    create .get method on the App class and implemented caching on client-side

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,19 +3,47 @@ import ReactDOM from 'react-dom';
 import ReviewContainer from './RatingsReviews/index.jsx';
 import Product from './ProductOverview/Product.jsx';
 import $ from 'jquery';
+import axios from 'axios';
+const Cache = require('../../util/cache.js');
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
+    this.cache = new Cache(600000);
+
     this.initialize = this.initialize.bind(this);
+    this.get = this.get.bind(this);
   }
 
   componentDidMount() {
     this.initialize();
   }
 
+  get(endpoint) {
+    const cached = this.cache.get(endpoint);
+
+    if (cached) {
+      return Promise.resolve(cached);
+    } else {
+      return axios.get('/api' + endpoint)
+        .then(response => {
+          this.cache.store(endpoint, response.data);
+          return response.data;
+        })
+        .catch(err => {
+          console.log("'GET' ERROR: ", err);
+          return null;
+        });
+    }
+  }
+
   initialize() {
+    this.get('/products')
+      .then(data => {
+        console.log('GOT SOME DATA: ', data);
+      });
+
     // $.ajax({
     //   method: 'GET',
     //   url: '/api/products',
