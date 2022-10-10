a0530074b18e31afe54106bf155345bf4992567a
Matt Alexander <dmalexander6@gmail.com>
2022-10-09 23:47:29 -0600

    more css

--- client/src/RelatedAndOutfit/ProductCard.jsx
+++ client/src/RelatedAndOutfit/ProductCard.jsx
@@ -4,11 +4,26 @@ import Stars from '../components/Stars.jsx';
 export default function Card(props) {
   const { item, style, reviewsMeta } = props;
 
+  function price() {
+    const price = item.default_price || style.original_price;
+    return price;
+  }
+
+  function starsAttr() {
+    const attributes = {};
+
+    if (reviewsMeta.ratings){
+      return { ratings: reviewsMeta.ratings };
+    } else {
+      return { stars: 0 };
+    }
+  }
+
   return <div className='card'>
     <img className='card-img' src={style.photos[0].thumbnail_url} />
-    {item.category}
+    <h5>{item.category}</h5>
     <h4>{item.name}</h4>
-    <p>{item.default_price}</p>
-    <Stars ratings={reviewsMeta.ratings} />
+    <p>{price()}</p>
+    <Stars attributes={starsAttr()} />
   </div>
 }
