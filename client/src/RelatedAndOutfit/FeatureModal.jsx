dafdd10d13ffc955b7c4887d48438bc9a62eecc2
Matt Alexander <dmalexander6@gmail.com>
2022-10-10 23:06:45 -0600

    initial modal component

--- client/src/RelatedAndOutfit/FeatureModal.jsx
+++ client/src/RelatedAndOutfit/FeatureModal.jsx
@@ -1,16 +1,20 @@
+import React, { useState, useEffect } from 'react';
 
 function parseFeatures(prod1, prod2) {
   // compare features of both products and return an object with combined features
   const features1 = prod1.features;
   const features2 = prod2.features;
-  const results = {};
+  const results = { _id: {
+    prod1: prod1.id,
+    prod2: prod2.id
+  }};
 
-  for (feature in features1) {
+  for (feature of features1) {
     const parsedFeature = results[feature.feature] || {};
 
     results[feature.feature] = Object.assign(parsedFeature, {product1: feature.value});
   }
-  for (feature in features2) {
+  for (feature of features2) {
     const parsedFeature = results[feature.feature] || {};
 
     results[feature.feature] = Object.assign(parsedFeature, {product2: feature.value});
@@ -19,19 +23,32 @@ function parseFeatures(prod1, prod2) {
   return results;
 }
 
-export default function FeatureModal = ({handleClose, show, product1, product2}) => {
+function productsChanged(featureData, prod1, prod2) {
+  return prod1.id !== featureData._id.prod1 || prod2.id !== featureData._id.prod2;
+}
+
+export default function FeatureModal({handleClose, show, product1, product2}) {
+  const [featureData, setFeatureData] = useState({});
+
+  useEffect(() => {
+    if (featureData._id && productsChanged(featureData, product1, product2)) {
+      setFeatureData(parseFeatures(product1, product2));
+      console.log('PARSED FEATURES: ', featuresData);
+    }
+  });
+
   const showHideClassName = show ? "modal display-block" : "modal display-none";
-  const featuresData = parseFeatures(product1, product2);
-  const features = Object.keys(featuresData);
+  console.log('RelatedAndOutfit: SHOWING MODAL', product1, product2);
+  const features = Object.keys(featureData);
 
 
   return <table className={showHideClassName}>
     <thead>
-      <tr><td colSpan={2}>{product1.name</td><td colSpan={2}>{product2.name}</td></tr>
+      <tr><td colSpan={2}>{product1.name}</td><td colSpan={2}>{product2.name}</td></tr>
     </thead>
     <tbody>
       {features.map((feature, i) => {
-        <tr key={i}><td>{featuresData[feature].product1}</td><td colSpan{2}>{feature}</td><td>{featuresData[feature].product2}</td></tr>
+        <tr key={i}><td>{featuresData[feature].product1}</td><td colSpan={2}>{feature}</td><td>{featuresData[feature].product2}</td></tr>
       })}
     </tbody>
   </table>
--- client/src/RelatedAndOutfit/ProductCard.jsx
+++ client/src/RelatedAndOutfit/ProductCard.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stars from '../components/Stars.jsx';
+import FeatureModal from './FeatureModal.jsx';
 
 export default function Card({ mainProduct, item, style, reviewsMeta }) {
+  const [showModal, setShowModal] = useState(false);
 
   function price() {
     const price = item.default_price || style.original_price;
@@ -20,11 +22,21 @@ export default function Card({ mainProduct, item, style, reviewsMeta }) {
     }
   }
 
-  return <div className='card'>
+  function handleToggleModal(e) {
+    if (showModal) {
+      setShowModal(false);
+    } else {
+      setShowModal(true);
+    }
+  }
+
+  return <><div className='card' onClick={handleToggleModal}>
     <img className='card-img' src={style.photos[0].thumbnail_url} />
     <h5>{item.category}</h5>
     <h4>{item.name}</h4>
     {price()}
     {stars()}
   </div>
+  {showModal ? <FeatureModal handleClose={handleToggleModal} show={true} product1={mainProduct} product2={item} /> : null}
+  </>
 }
