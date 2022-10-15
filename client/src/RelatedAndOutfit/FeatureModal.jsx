c7e7530282ef10c7a967b1ce8d80d261f0587b9c
Matt Alexander <dmalexander6@gmail.com>
2022-10-15 15:33:56 -0600

    couple css changes

--- client/src/RelatedAndOutfit/FeatureModal.jsx
+++ client/src/RelatedAndOutfit/FeatureModal.jsx
@@ -12,9 +12,8 @@ function parseFeatures(prod1, prod2) {
       prod2: prod2.id
     }
   };
-  console.log('PARSEFEATURES', features1, features2);
+
   for (let feature of features1) {
-    console.log('FEATURE: ', feature);
     const parsedFeature = results[feature.feature] || {};
 
     results[feature.feature] = Object.assign(parsedFeature, { product1: feature.value });
@@ -31,8 +30,7 @@ function parseFeatures(prod1, prod2) {
 function productsChanged(featureData, prod1 = {}, prod2 = {}) {
   if (!prod1.id || !prod2.id) { return false; }
   if (!featureData._id) { return true; }
-  const changed = prod1.id !== featureData._id.prod1 || prod2.id !== featureData._id.prod2;
-  console.log('MODAL: PRODUCT CHANGED: ', changed);
+
   return prod1.id !== featureData._id.prod1 || prod2.id !== featureData._id.prod2;
 }
 
@@ -54,18 +52,18 @@ export default function FeatureModal({ product1, get }) {
   const [isHidden, setIsHidden] = useState(true);
   const modalRef = useRef();
   const clickedOutside = (e) => {
-    console.log('HANDLE CLICK', e);
-    console.log('CHECK REF', modalRef);
+    //console.log('HANDLE CLICK', e);
+    //console.log('CHECK REF', modalRef);
     if (modalRef.current && !modalRef.current.contains(e.target)) {
       setIsHidden(true);
     }
   };
   const handleClick = (e) => {
-    console.log('---> handleClick', getProductCardElement(e));
+    //console.log('---> handleClick', getProductCardElement(e));
     const cardElement = getProductCardElement(e);
     if (isHidden && cardElement) {
       const productId = cardElement.getAttribute('data-id');
-      console.log('PRODUCT ID: ', productId, typeof get);
+      //console.log('PRODUCT ID: ', productId, typeof get);
       get('/products/'.concat(productId))
         .then((data) => {
           console.log('DATA', data);
@@ -87,15 +85,15 @@ export default function FeatureModal({ product1, get }) {
   if (productToCompare === null) { return null; }
   if (productsChanged(featureData, product1, productToCompare)) {
     setFeatureData(parseFeatures(product1, productToCompare));
-    console.log('PARSED FEATURES: ', featureData);
+    //console.log('PARSED FEATURES: ', featureData);
     return null;
   }
 
   const features = Object.keys(featureData);
-  console.log('GOT FEATURES', features);
-  const tableClass = 'modal ' + (isHidden ? 'display-none' : 'display-initial');
+  //console.log('GOT FEATURES', features);
+  const tableClass = 'modal-table ' + (isHidden ? 'display-none' : 'display-initial');
 
-  return <table className={tableClass} ref={modalRef}>
+  return <div className="modal"><table className={tableClass} ref={modalRef}>
     <thead>
       <tr><th colSpan="2" className="table-head column-left">{product1.name}</th><th colSpan="2" className="table-head column-right">{productToCompare.name}</th></tr>
     </thead>
@@ -108,5 +106,5 @@ export default function FeatureModal({ product1, get }) {
         }
       })}
     </tbody>
-  </table>
+  </table></div>
 };
