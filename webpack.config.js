24a8ca9eb2e73c260734f7d362a649c56ddcbe5b
Connor Thurston <connor.thurston1@gmail.com>
2022-10-28 22:22:49 -0600

    changed webpack config to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.join(__dirname, "client/src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "client/dist"),
